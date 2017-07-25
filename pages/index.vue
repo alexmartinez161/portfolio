e184e16c12757da14ec226f75b3b71ee9b81981d
3goliad <3goliad@gmail.com>
2017-07-25 21:17:39 +0000

    index work

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,15 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
       <h1 class="title">
-        NUXT
+        Alex Martinez
       </h1>
-      <h2 class="subtitle">
-        Universal Vue.js Application
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+      <div id="bio">
+        bluj
       </div>
     </div>
   </section>
 </template>
-
-<script>
-import Logo from '~components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
 .container
